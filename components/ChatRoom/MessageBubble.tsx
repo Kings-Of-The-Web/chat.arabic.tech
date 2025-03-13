4074faad942983a15ef36f80df7c678cc8c9ec01
Kings Of The Web <kingsofthewebofficial@gmail.com>
2025-03-13 08:36:02 -0400

    Run format

--- components/ChatRoom/MessageBubble.tsx
+++ components/ChatRoom/MessageBubble.tsx
@@ -1,11 +1,7 @@
 import { format } from 'date-fns';
 import { ar } from 'date-fns/locale';
-import {
-    Tooltip,
-    TooltipContent,
-    TooltipProvider,
-    TooltipTrigger,
-} from '@/components/ui/tooltip';
+
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip';
 
 interface MessageBubbleProps {
     message: App.Message;
@@ -24,7 +20,7 @@ export function MessageBubble({ message, isOwnMessage }: MessageBubbleProps) {
             >
                 <div className="mb-1 text-sm font-bold">{message.userId}</div>
                 <div className="mb-1">{message.body}</div>
-                <div className="text-xs opacity-90 cursor-pointer">
+                <div className="cursor-pointer text-xs opacity-90">
                     {format(message.timestamp, 'p', { locale: ar })}
                     {!isOwnMessage && message.isRead && message.isReadAt && (
                         <TooltipProvider>
@@ -36,7 +32,8 @@ export function MessageBubble({ message, isOwnMessage }: MessageBubbleProps) {
                                 </TooltipTrigger>
                                 <TooltipContent>
                                     <p className="font-hacen">
-                                        تم القراءة في {format(message.isReadAt, 'p', { locale: ar })}
+                                        تم القراءة في{' '}
+                                        {format(message.isReadAt, 'p', { locale: ar })}
                                     </p>
                                 </TooltipContent>
                             </Tooltip>
